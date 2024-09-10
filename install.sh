eec48c6a19e56be02fa998905ceb25a73f7f9f20
Bside182 <65766638+Bside182@users.noreply.github.com>
2024-09-10 16:19:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+apt-get update && \
+apt-get install -y \
+unzip
 
 wget https://releases.hashicorp.com/terraform/1.9.2/terraform_1.9.2_linux_amd64.zip && \
     unzip terraform*.zip && \
