1a1f18a9f0987c172e85df040e7b667ba9da2d82
Bside182 <65766638+Bside182@users.noreply.github.com>
2024-09-10 17:08:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,11 @@ apt-get update && \
 apt-get install -y \
 unzip
 
- wget https://releases.hashicorp.com/terraform/'.current_version'/'.current_version'_linux_amd64.zip && \
+wget https://checkpoint-api.hashicorp.com/v1/check/terraform -O /tmp/checkpoint.json && \
+LATEST_VERSION=$(jq -r -M '.current_version' /tmp/checkpoint.json) && \
+wget https://releases.hashicorp.com/terraform/'.current_version'/'.current_version'_linux_amd64.zip && \
 unzip terraform*.zip && \
- mv terraform /usr/local/bin
-    
-wget https://releases.hashicorp.com/terraform/1.9.2/terraform_1.9.2_linux_amd64.zip && \
-    unzip terraform*.zip && \
-    mv terraform /usr/local/bin
+mv terraform /usr/local/bin
     
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && \
     dpkg -i packages-microsoft-prod.deb
